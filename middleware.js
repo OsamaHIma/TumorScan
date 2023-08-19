fdc8183461e577c19cf7938f2ba3eb8e02202c22
Osama Ibrahim <108794970+OsamaHIma@users.noreply.github.com>
2023-08-19 23:19:27 +0300

    push

--- middleware.js
+++ middleware.js
@@ -1,12 +1,13 @@
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
+import { toast } from "react-toastify";
 
 export async function middleware(req) {
   const url = new URL(req.url);
   const pathname = req.nextUrl.pathname;
   const searchParams = url.searchParams;
-  const mode = searchParams.get('mode');
-  const oobCode = searchParams.get('oobCode');
+  const mode = searchParams.get("mode");
+  const oobCode = searchParams.get("oobCode");
 
   // if (pathname === "/verify-email" || pathname === "/auth/reset-password") {
   //   if (mode !== "verifyEmail" && mode !== "resetPassword") {
@@ -15,11 +16,14 @@ export async function middleware(req) {
   // }
 
   if (mode === "verifyEmail") {
-    return NextResponse.redirect(new URL(`/verify-email?oobCode=${oobCode}`, req.url));
+    return NextResponse.redirect(
+      new URL(`/verify-email?oobCode=${oobCode}`, req.url)
+    );
   } else if (mode === "resetPassword") {
-    return NextResponse.redirect(new URL(`/auth/reset-password?oobCode=${oobCode}`, req.url));
+    return NextResponse.redirect(
+      new URL(`/auth/reset-password?oobCode=${oobCode}`, req.url)
+    );
   }
-
 
   const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
@@ -29,12 +33,25 @@ export async function middleware(req) {
     }
   }
   if (session) {
-    if (pathname.includes("/auth")) {
-      return NextResponse.redirect(new URL("/upload", req.url));
+    if (session.user.user.emailVerified) {
+      if (pathname.includes("/auth")) {
+        return NextResponse.redirect(new URL("/upload", req.url));
+      }
+    } else {
+      toast.error("Your email is not verified, please verify it to upload images");
+      return NextResponse.redirect(new URL("/", req.url));
     }
+
     return NextResponse.next();
   }
 }
 export const config = {
-  matcher: ["/","/verify-email", "/dashboard/:path*", "/auth/login", "/dashboard","/upload"],
+  matcher: [
+    "/",
+    "/verify-email",
+    "/dashboard/:path*",
+    "/auth/login",
+    "/dashboard",
+    "/upload",
+  ],
 };
