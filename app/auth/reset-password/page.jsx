b039a59f3b5aa38add497221faa89f2ae0c36ca4
Osama Ibrahim <108794970+OsamaHIma@users.noreply.github.com>
2023-08-19 20:11:43 +0300

    fix resst password

--- app/auth/reset-password/page.jsx
+++ app/auth/reset-password/page.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 import { useSearchParams } from 'next/navigation';
 import { resetPassword } from "@/lib/firebase";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 
 const ResetPasswordPage = () => {
   const searchParams = useSearchParams();
@@ -12,6 +13,7 @@ const ResetPasswordPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(false);
+  const [passwordIcon, setPasswordIcon] = useState(false);
 
   const handleResetPassword = async (e) => {
     e.preventDefault();
@@ -22,7 +24,7 @@ const ResetPasswordPage = () => {
     setError(null);
     setIsLoading(true);
     try {
-      const res = await resetPassword(password)
+      const res = await resetPassword(oobCode,password)
       setSuccess(true);
     } catch (error) {
       setError(error.code);
@@ -30,7 +32,9 @@ const ResetPasswordPage = () => {
       setIsLoading(false);
     }
   };
-
+  const togglePasswordIcon = () => {
+    setPasswordIcon(!passwordIcon);
+  };
   return (
     <div className="flex flex-col items-center justify-center mt-10 md:mt-20">
       <h1 className="text-3xl font-bold mb-4">
@@ -38,7 +42,34 @@ const ResetPasswordPage = () => {
       </h1>
       {!success ? (
         <form className="flex flex-col gap-4 w-full max-w-md" onSubmit={handleResetPassword}>
-          <label htmlFor="password" className="text-lg font-semibold">
+           <label htmlFor="password" className="text-lg font-semibold">
+            New password:
+          </label>
+          <div className="relative w-full">
+            <input
+              type={passwordIcon ? "text" : "password"}
+              placeholder="Enter Your Password"
+              id="password"
+              name="password"
+              value={password}
+              minLength={8}
+              onChange={(e) => setPassword(e.target.value)}
+              className={`px-4 w-full rounded-md bg-indigo-300/70 dark:bg-slate-800 placeholder:text-slate-50 focus:outline-gray-200 py-2 ${error && "border-red-500"
+                }`}
+            />
+            {passwordIcon ? (
+              <EyeIcon
+                className="cursor-pointer top-[50%] translate-y-[-50%] absolute right-2 text-gray-400"
+                onClick={togglePasswordIcon}
+              />
+            ) : (
+              <EyeOffIcon
+                className="cursor-pointer top-[50%] translate-y-[-50%] absolute right-2 text-gray-400"
+                onClick={togglePasswordIcon}
+              />
+            )}
+            </div>
+          {/* <label htmlFor="password" className="text-lg font-semibold">
             New password:
           </label>
           <input
@@ -50,12 +81,12 @@ const ResetPasswordPage = () => {
             onChange={(e) => setPassword(e.target.value)}
             className={`px-4 w-full rounded-md bg-indigo-300/70 dark:bg-slate-800 placeholder:text-slate-50 focus:outline-gray-200 py-2 ${error && "border-red-500"
               }`}
-          />
+          /> */}
           <label htmlFor="confirmPassword" className="text-lg font-semibold">
             Confirm new password:
           </label>
           <input
-            type="password"
+           type={passwordIcon ? "text" : "password"}
             id="confirmPassword"
             minLength={8}
             placeholder="Confirm Your Password"
