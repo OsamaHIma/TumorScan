94989306359762c24ab7ceb52935068c7c978046
Osama Ibrahim <108794970+OsamaHIma@users.noreply.github.com>
2023-09-04 21:25:36 +0300

    Update GetStarted.jsx

--- home/GetStarted.jsx
+++ home/GetStarted.jsx
@@ -8,7 +8,7 @@ const GetStarted = () => {
     <section className="paddings innerWidth" id="get-started">
       <div className="flexColCenter w-full gap-6 rounded-md border-4 border-indigo-700 bg-indigo-600 px-6 py-10 text-center md:px-0">
         <h1 className="text-xl font-semibold text-white md:text-4xl">
-          <Translate>Get started with</Translate> Tumor scanner
+          <Translate>Get started with</Translate> Tumor scan
         </h1>
         <p className="text-gray-300">
           <Translate>
